c67576caee238c51d6939a6d5698f623bf11199a
Darwin Pinheiro <darwinop@gmail.com>
2020-06-20 16:14:18 -0300

    Configurando Mongo

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+//Iniciando o App
 const app = express();
 
+//Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/db_gentlemen',{ useNewUrlParser: true});
+
+//Rotas
 app.get('/',(req,res) =>{
      res.send('Olá Darwin!');
 });
